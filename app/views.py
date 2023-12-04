30c8077be674032eeb97fe51b1c2ddd633688d58
Haris Takam <takamharis2@gmail.com>
2023-12-04 10:51:05 +0100

    Ajout de la fonctionnalité d'authentification (Connexion)

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authentication
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in !")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logging in, please again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
