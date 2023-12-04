708c287d6f1fdb6bb461b141658d9fdf44ec7dcc
Haris Takam <takamharis2@gmail.com>
2023-12-05 00:59:51 +0100

    Ajout de la fonctionnalité d'authentification (Register )

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from app.forms import SignUpForm
+
 
 # Create your views here.
 def home(request):
@@ -14,10 +16,34 @@ def home(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "You have been logged in !")
-            return redirect('home')
+            messages.success(request, f"You have been logged in {username} !")
+            return render(request, 'home.html', {'name': username})
         else:
             messages.error(request, "There was an error logging in, please again")
             return redirect('home')
     else:
         return render(request, 'home.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out  !")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully register ! Welcome 😊")
+            return redirect('home')
+    else:
+        form = SignUpForm
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
